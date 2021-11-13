3698e96991fd1ce3f58dfd177fe95fe68c503715
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-13 09:37:07 +0700

    Refactor display message related to its own function

--- app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
@@ -8,20 +8,24 @@ class PreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+        displayMessage()
+    }
+
+    private fun displayMessage() {
         val message = intent.getSerializableExtra("Message") as Message
         val messagePreviewText = """
-            Hi ${message.contactName},
-            
-            My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
-            
-            I have a portfolio of apps to demonstrate my technical skills that I can show on request.
-            
-            I am able to start a new position ${message.getAvailability()}.
-            
-            Please get in touch if you have any suitable roles for me.
-            
-            Thanks and best regards.
-        """.trimIndent()
+                Hi ${message.contactName},
+                
+                My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
+                
+                I have a portfolio of apps to demonstrate my technical skills that I can show on request.
+                
+                I am able to start a new position ${message.getAvailability()}.
+                
+                Please get in touch if you have any suitable roles for me.
+                
+                Thanks and best regards.
+            """.trimIndent()
         text_view_messsage.text = messagePreviewText
     }
 }

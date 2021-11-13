4b10a28624dcd4de25a8784e9d8b3a886f0a0d1a
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-13 10:10:42 +0700

    Setup send message button

--- app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
@@ -1,19 +1,25 @@
 package com.jeffryhermanto.selfpromoapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
         displayMessage()
+        setupButton()
     }
 
     private fun displayMessage() {
-        val message = intent.getSerializableExtra("Message") as Message
-        val messagePreviewText = """
+        message = intent.getSerializableExtra("Message") as Message
+        messagePreviewText = """
                 Hi ${message.contactName},
                 
                 My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
@@ -27,5 +33,15 @@ class PreviewActivity : AppCompatActivity() {
                 Thanks and best regards.
             """.trimIndent()
         text_view_messsage.text = messagePreviewText
+    }
+
+    private fun setupButton() {
+        button_send_message.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto: ${message.contactNumber}")
+                putExtra("sms_body", messagePreviewText)
+            }
+            startActivity(intent)
+        }
     }
 }

e0eba82e3857958fb991174ee2635e3c7bc23794
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-13 07:30:23 +0700

    Passing data to PreviewActivity

--- app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
@@ -7,5 +7,13 @@ class PreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+
+        val contactName = intent.getStringExtra("Contact Name")
+        val contactNumber = intent.getStringExtra("Contact Number")
+        val myDisplayName = intent.getStringExtra("My Display Name")
+        val includeJunior = intent.getBooleanExtra("Include Junior", false)
+        val jobTitle = intent.getStringExtra("Job Title")
+        val immediateStart = intent.getBooleanExtra("Immediate Start", false)
+        val startDate = intent.getStringExtra("Start Date")
     }
 }

af0d7d0669b3978188256d37b8fa296ce6d0f130
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-13 09:33:11 +0700

    Displaying our Message - Kotlin Raw Strings and One-Liner If Statements

--- app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/jeffryhermanto/selfpromoapp/PreviewActivity.kt
@@ -2,11 +2,26 @@ package com.jeffryhermanto.selfpromoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
         val message = intent.getSerializableExtra("Message") as Message
+        val messagePreviewText = """
+            Hi ${message.contactName},
+            
+            My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
+            
+            I have a portfolio of apps to demonstrate my technical skills that I can show on request.
+            
+            I am able to start a new position ${message.getAvailability()}.
+            
+            Please get in touch if you have any suitable roles for me.
+            
+            Thanks and best regards.
+        """.trimIndent()
+        text_view_messsage.text = messagePreviewText
     }
 }
